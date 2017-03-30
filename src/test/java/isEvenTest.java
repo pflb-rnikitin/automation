c7af5f4dcfa776f051f698ad3839f79c73c7b6a5
bashoku <r.nikitin@pflb.ru>
2017-03-30 10:13:15 +0400

    dataprovider+hamcrest commit

--- src/test/java/isEvenTest.java
+++ src/test/java/isEvenTest.java
@@ -11,45 +11,27 @@ public class isEvenTest {
 
     @DataProvider (name="TestData")
     public Object[][] getData () {
-        return new Object[][]{
-                new Object[]{41},
-                new Object[]{234},
-                new Object[]{-23543},
-                new Object[]{0},
-                new Object[]{9999999},
-                new Object[]{41},
-                new Object[]{41},
-                new Object[]{41},
-                new Object[]{41},
-                new Object[]{41},
-                new Object[]{41}
-        };
-    }
-        /*Object[][]data = new Object[10][0];
-        data [0][0] = 1;
-        data [1][0] = 5;
-        data [2][0] = 500;
-        data [3][0] = 6312;
-        data [4][0] = -4326;
-        data [5][0] = 234732;
-        data [6][0] = 0;
-        data [7][0] = 234;
-        data [8][0] = 242;
-        data [9][0] = 4;
-        data [10][0] = 4;
-        return data;*/
+        Object [][] data = new Object[5][2];
+                data[0][0]=424231;
+                data[0][1]=false;
 
+                data[1][0]=0;
+                data[1][1]=true;
+
+                data[2][0]=89;
+                data[2][1]=true;
+
+                data[3][0]=-101;
+                data[3][1]=true;
+
+                data[4][0]=423432131;
+                data[4][1]=true;
+        return data;
+    }
 
     @Test (dataProvider = "TestData")
-    public void testCheckIsEven(int m) throws Exception {
-        MatcherAssert.assertThat(sut.check(m), is(true));
+    public void testCheckIsEven(int m, boolean b) throws Exception {
+        MatcherAssert.assertThat(sut.check(m), is(b));
         System.out.println(m);
-        MatcherAssert.assertThat(sut.check(m), is(true));
-        /*MatcherAssert.assertThat(sut.check1(5), is(false));
-        MatcherAssert.assertThat(sut.check1(-900), is(true));
-        MatcherAssert.assertThat(sut.check1(0), is(true));
-        MatcherAssert.assertThat(sut.incorrectCheck(5), is(false));
-        MatcherAssert.assertThat(sut.incorrectCheck(-900), is(true));
-        MatcherAssert.assertThat(sut.incorrectCheck(0), is(true));*/
     }
 }
